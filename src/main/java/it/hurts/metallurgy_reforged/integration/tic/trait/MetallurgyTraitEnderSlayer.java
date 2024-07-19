2a17558de90b57a1e3d6e6caedc5a4efb739465b
Sweebow <evan.jeebow@gmail.com>
2024-07-19 14:13:39 -0600

    ender slayer

---
+++ src/main/java/it/hurts/metallurgy_reforged/integration/tic/trait/MetallurgyTraitEnderSlayer.java
@@ -0,0 +1,45 @@
+/*==============================================================================
+ = Class: MetallurgyTraitDecay
+ = This class is part of Metallurgy 4: Reforged
+ = Complete source code is available at https://github.com/Davoleo/Metallurgy-4-Reforged
+ = This code is licensed under GNU GPLv3
+ = Authors: Davoleo, ItHurtsLikeHell, PierKnight100
+ = Copyright (c) 2018-2021.
+ =============================================================================*/
+
+package it.hurts.metallurgy_reforged.integration.tic.trait;
+
+import it.hurts.metallurgy_reforged.util.Utils;
+import net.minecraft.entity.EntityList;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.ItemStack;
+import slimeknights.tconstruct.library.traits.AbstractTrait;
+
+import javax.annotation.Nullable;
+
+public class MetallurgyTraitEnderSlayer extends AbstractTrait implements IMetallurgyTrait {
+
+	public MetallurgyTraitEnderSlayer()
+	{
+		super("decay_trait", 0xFF575000);
+		this.register("metallurgy.trait.enderslayer", "metallurgy.trait.enderslayer.tooltip");
+	}
+
+	 @Override
+	 public float damage(ItemStack tool, EntityLivingBase player, EntityLivingBase target, float damage, float newDamage, boolean isCritical) {
+		if (EntityList.getKey(target).toString().equals("minecraft:enderman") || EntityList.getKey(target).toString().equals("minecraft:shulker") || EntityList.getKey(target).toString().equals("minecraft:ender_dragon"))
+			return (newDamage * 1.5f);
+		else if (target.world.provider.getDimension() == 1)
+			return (newDamage * 1.5f);
+		return newDamage;
+	 }
+	
+	@Override
+	public void register(String name, @Nullable String tooltip)
+	{
+		Utils.localizeEscapingCustomSequences(String.format(LOC_Name, name));
+		if (tooltip != null)
+			Utils.localizeEscapingCustomSequences(String.format(LOC_Name, tooltip));
+	}
+
+}
